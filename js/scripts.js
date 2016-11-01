2819e0405fc7e2f1c9da50ac46c548077cd53d2f
Kevin Finley and Stephan Khoury <stephan@>
2016-11-01 11:58:08 -0700

    add object and jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
-function Ticket(movie, time, age) {
+var newReleases = ["Avatar", "Life"];
+
+
+function Ticket(movie, time, age, Price) {
   this.movie = movie;
   this.time = time;
   this.age = age;
+  this.price = 6;
 }
+Ticket.prototype.addToPrice = function () {
+  if(newReleases.includes(this.movie)) {
+    this.price += 4;
+  }
+};
+
+
+
+$(document).ready(function() {
+  $("#inputForm").submit(function(event) {
+    event.preventDefault();
+
+
+    var movie = $("#movie").val();
+    var time = $("#time").val();
+    var age = $("#age").val();
+    var price = 10;
+
+    var newTicket = new Ticket(movie, time, age, price);
+    newTicket.addToPrice();
+
+
+    $("#result").text(newTicket)
+    console.log(newTicket)
+  });
+});
